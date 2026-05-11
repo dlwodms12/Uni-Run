172da648aeffc8a33cffe23ce667cd27cd18efa4
Jae Eun <pdw05498@naver.com>
2026-05-12 05:54:22 +0900

    싱글턴 패턴 게임 매니저 제작
    1. 단 한개의 게임 매니저 인스턴스만 존재하도록 코드 작성
    2. 게임 오버 상태에서 게임을 재시작할 수 있는 기능
    3. 점수를 증가시키는 함수
    4. 캐릭터 사망 시 게임 오버를 실행하는 함수

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -63,6 +63,9 @@ public class PlayerController : MonoBehaviour {
         playerRigidbody.linearVelocity = Vector2.zero;
         //사망 상태 = true
         isDead = true;
+
+        //게임 매니저의 게임 오버 처리 실행
+        GameManager.instance.OnPlayerDead();
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
